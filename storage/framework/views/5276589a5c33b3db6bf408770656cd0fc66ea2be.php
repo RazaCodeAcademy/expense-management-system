277ffe1446001f68109ada9e892fc6a25ec81e90
mhusnainraza512 <sjsolutionz>
2023-01-22 18:06:03 +0500

    updated

---
+++ storage/framework/views/5276589a5c33b3db6bf408770656cd0fc66ea2be.php
@@ -0,0 +1,168 @@
+<?php $__env->startSection('title', 'List Of Vouchers Requested For Approval'); ?>
+
+<?php $__env->startSection('content_header'); ?>
+    <h1>List Of Vouchers Requested For Approval</h1>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+    <?php $__env->startPush('css'); ?>
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="<?php echo e(asset('/plugins/fontawesome-free/css/all.min.css')); ?>">
+        <!-- DataTables -->
+        <link rel="stylesheet" href="<?php echo e(asset('/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')); ?>">
+        <link rel="stylesheet" href="<?php echo e(asset('/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')); ?>">
+        <link rel="stylesheet" href="<?php echo e(asset('/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')); ?>">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="<?php echo e(asset('/dist/css/adminlte.min.css')); ?>">
+    <?php $__env->stopPush(); ?>
+    <div class="card px-3 py-1">
+        <!-- /.card-header -->
+        <div class="card-body">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <label for="min">From :</label>
+                        <input type="date" id="min" onfocus="this.showPicker()" value="<?php echo e(date('Y-m-01')); ?>"
+                            class="form-control">
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <label for="max">To :</label>
+                        <input type="date" id="max" onfocus="this.showPicker()" value="<?php echo e(date('Y-m-d')); ?>"
+                            class="form-control">
+                    </div>
+                </div>
+            </div>
+            <table id="example1" class="table table-bordered table-striped">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Employee Name</th>
+                        <th scope="col">Job Numbers</th>
+                        <th scope="col">Voucher Number</th>
+                        <th scope="col">Voucher Date</th>
+                        <th scope="col">Proposed Amount</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $__currentLoopData = $vouchers; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $voucher): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <?php
+                        $expenses = $voucher->expenses;
+                        $total_amt = 0.0;
+                        foreach ($expenses as $exp) {
+                            $total_amt += $exp->amount;
+                        }
+
+                        ?>
+                        <tr>
+                            <td><?php echo e($voucher->employee()->first()->name); ?></td>
+                            <td>
+                                <?php $__currentLoopData = $voucher->jobs; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $job): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <?php echo e($job->number); ?><?php if(!$loop->last): ?>
+                                        ,
+                                    <?php endif; ?>
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                            </td>
+                            <td><?php echo e($voucher->number); ?></td>
+                            <td>
+                                <?php if(isset($voucher->date)): ?>
+                                    <?php echo e($voucher->date->format('Y-m-d')); ?>
+
+                                <?php endif; ?>
+                            </td>
+                            <td>
+                                <span class="badge badge-primary px-2 py-2">
+                                    Rs. <?php echo e($total_amt); ?>
+
+                                </span>
+                            </td>
+                            <td>
+                                <a class="btn btn-primary"
+                                    href="<?php echo e(route('employees.voucherDetails', ['id' => $voucher->id])); ?>">View</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+    <?php $__env->startPush('js'); ?>
+        <!-- DataTables  & Plugins -->
+        <script src="<?php echo e(asset('/plugins/datatables/jquery.dataTables.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('/plugins/datatables-responsive/js/dataTables.responsive.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('/plugins/datatables-buttons/js/dataTables.buttons.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('/plugins/jszip/jszip.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('/plugins/pdfmake/pdfmake.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('/plugins/pdfmake/vfs_fonts.js')); ?>"></script>
+        <script src="<?php echo e(asset('/plugins/datatables-buttons/js/buttons.html5.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('/plugins/datatables-buttons/js/buttons.print.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('/plugins/datatables-buttons/js/buttons.colVis.min.js')); ?>"></script>
+        <!-- AdminLTE App -->
+        <script src="<?php echo e(asset('/dist/js/adminlte.min.js')); ?>"></script>
+        <script>
+            $(function() {
+                $("#example1").DataTable({
+                    "order": [[1, 'desc']],
+                    "oSearch": {
+                    "sSearch": "<?php echo e(date('Y-m')); ?>"
+                    },
+                    "responsive": true,
+                    "lengthChange": false,
+                    "autoWidth": false,
+                    "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+                }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+                $('#example2').DataTable({
+                    "paging": true,
+                    "lengthChange": false,
+                    "searching": false,
+                    "ordering": true,
+                    "info": true,
+                    "autoWidth": false,
+                    "responsive": true,
+                });
+
+                $.fn.dataTable.ext.search.push(
+                    function(settings, data, dataIndex) {
+                        $('input[type="search"]').val('').keyup();
+                        var FilterStart = $('#min').val();
+                        var FilterEnd = $('#max').val();
+                        var DataTableStart = data[3].trim();
+                        var DataTableEnd = data[3].trim();
+                        if (FilterStart == '' || FilterEnd == '') {
+                            return true;
+                        }
+                        if (DataTableStart >= FilterStart && DataTableEnd <= FilterEnd) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+
+                    });
+                // --------------------------
+                $('#min').change(function(e) {
+                    var Table = $("#example1").DataTable();
+                    Table.draw();
+
+                });
+                $('#max').change(function(e) {
+                    var Table = $("#example1").DataTable();
+                    Table.draw();
+
+                });
+            });
+        </script>
+    <?php $__env->stopPush(); ?>
+<?php $__env->stopSection(); ?>
+
+
+
+<?php $__env->startSection('js'); ?>
+    <script src="<?php echo e(asset('js/tableFilter.js')); ?>"></script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('adminlte::page', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\freelancing\employee-expense-management-system\resources\views/employees/approval.blade.php ENDPATH**/ ?>
